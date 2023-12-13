dfc767805152d8f724cc94c951ffdc90dd766c8c
LeandroL18 <leandrolipani6@gmail.com>
2023-12-13 13:40:40 +0100

    Structure default

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+// Attraverso l’apposita API generare 10 indirizzi email e stamparli in pagina all’interno di una lista
+// Bonus: Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    }
+}).mount('#app')
+
