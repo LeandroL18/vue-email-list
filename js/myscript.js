8d7a6eb89bfe51e87d4e6710515883beaf4dc9f7
LeandroL18 <leandrolipani6@gmail.com>
2023-12-13 14:37:12 +0100

    email generation and printing

--- js/myscript.js
+++ js/myscript.js
@@ -6,14 +6,23 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-
+            emails: []
         }
     },
     methods:{
 
     },
     mounted(){
+        console.log("L'applicazione è caricata!");
 
+        for(let i=0; i <= 9; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((email) => {
+                console.log(email.data.response);
+                this.emails.push(email.data.response);
+                console.log(this.emails);    
+            })
+        }
     }
 }).mount('#app')
 
